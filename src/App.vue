3db8ac0454bd24b9e7352b29179aa168db59d207
Lustach <lusta.vlad2001@gmail.com>
2019-12-02 23:23:18 +0300

    Начало

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,73 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+  <div>
+        <v-layout>
+
+        <v-toolbar color="primary">
+            <v-btn text icon color="white">
+              <v-icon>mdi-menu</v-icon><!-- logo -->
+            </v-btn>
+
+          <v-col cols="2" class="mt-2">
+            <v-text-field
+              placeholder="Search..."
+              solo
+            ></v-text-field>
+          </v-col>
+            <v-btn text rounded color="white">
+              <span>Я ментор</span>
+            </v-btn>
+            <v-btn text rounded color="white">
+              <span>Я ищу ментора</span>
+            </v-btn>
+          <v-spacer></v-spacer>
+
+            <v-btn text rounded color="white">
+              <router-link to="/login"><span>Sign in</span></router-link>
+            </v-btn>
+            <v-btn text rounded color="white">
+              <span>Sign up</span>
+            </v-btn>
+
+        </v-toolbar>
+
+        </v-layout>
+        <v-flex>
+        <v-layout row wrap>
+          
+        </v-layout>
+      </v-flex>
+      </div>
+    
+    <v-content>
+      <!-- <Registration/> -->
+      <router-view/>
+      <!-- <Registration/> -->
+    </v-content>
+  </v-app>
 </template>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import HelloWorld from './components/HelloWorld'
+import Registration from './components/Registration'
+export default {
+  name: 'App',
 
-#nav {
-  padding: 30px;
-}
+  components: {
+    HelloWorld,
+    Registration
+  },
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
+  data: () => ({
+    //
+  }),
+};
+</script>
+<style lang="scss">
+.v-text-field__details{
+  display:none;
 }
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+// .container{
+//   display:none;
+// }
 </style>
---
+++ src/components/Registration.vue
@@ -0,0 +1,67 @@
+<template>
+    <div>
+        <v-flex>
+        <v-flex row justify-center pa-5>
+            <span class="display-2">Create your account</span>
+        </v-flex>
+        <v-flex>
+            <v-card
+    class="mx-auto"
+    max-width="644"
+    elevation="5"
+  >
+  <v-progress-linear rounded striped :value="progressValue" height="9"></v-progress-linear>
+    <v-card-text>
+      <div></div>
+      <p class="display-1 text--primary">
+        <!-- be•nev•o•lent -->
+        <v-col cols="12">
+            <v-text-field
+                label="Password"
+
+                outlined
+            ></v-text-field>
+          </v-col>
+      </p>
+      <p>
+        <v-col cols="12">
+            <v-text-field
+                label="Username"
+                outlined
+                
+            ></v-text-field>
+          </v-col>
+          </p>
+          <p>
+        <v-col cols="12">
+            <v-text-field
+                label="E-mail"
+                outlined
+                
+            ></v-text-field>
+          </v-col>
+          </p>
+    </v-card-text>
+    <v-card-actions>
+      <v-btn
+        text
+        color="deep-purple accent-4"
+      >
+        Learn More
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+        </v-flex>
+        </v-flex>
+    </div>
+</template>
+<script>
+export default {
+    data:()=>({
+        progressValue: 0
+    }),
+    methods:{
+        
+    }
+}
+</script>
